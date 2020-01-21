376e250a42a7504b69b3c3050aac79b5a2e00fe6
Andy Liang <andyzen619@gmail.com>
2020-01-21 12:44:03 -0500

    Add db folder, add todo list schema

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+Drop table if EXISTS todoListItems cascade;
+
+create table todoListItems (
+  id serial primary key,
+  title varchar(255),
+  description varchar(1028),
+  due_date DATE,
+  status varchar(255)
+);
