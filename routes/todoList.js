fc79dffae028675455506f2bbd872a2b4dc01037
Andy Liang <andyzen619@gmail.com>
2020-01-21 13:36:10 -0500

    HTTP request work

--- routes/todoList.js
+++ routes/todoList.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('../db/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,19 @@ router.get('/', function(req, res, next) {
 router.get('/getAll', (req, res) => {
   res.send('All TODO items are here!!');
 })
+
+router.post('/add', (req, res) => {
+  res.send('Add todo item!');
+});
+
+router.put('/update/:id', (req, res) => {
+  const todoItemId = req.params.id;
+  res.send(`Update todo item ${todoItemId}`);
+});
+
+router.delete('/remove/:id', (req, res) =>{
+  const todoItemId = req.params.id;
+  res.send(`Remove item ${todoItemId}`);
+});
 
 module.exports = router;
