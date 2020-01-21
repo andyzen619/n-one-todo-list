d27ce544648a8f738701a323a0f03cecfcb9fff7
Andy Liang <andyzen619@gmail.com>
2020-01-21 16:38:10 -0500

    Add sort by due date functionality

--- routes/todoList.js
+++ routes/todoList.js
@@ -116,4 +116,18 @@ router.get("/completed", (req, res) => {
   });
 });
 
+/**
+ * Gets all tasks sorted by due date descending
+ */
+router.get('/getAll/sortByDueDate', (req, res) => {
+  pool.query(`select * from todolistitems order by due_date ASC`)
+  .then(q_res => {
+
+    res.send(q_res.rows);
+  })
+  .catch(q_err => {
+    res.send("There was an error processing this request. Please try again.");
+  })
+});
+
 module.exports = router;
